d8c917e7302af093bb1c10648d14afda5b1380e5
Robert <r.mill@ntlworld.com>
2017-11-05 11:56:18 +0000

    third update4

---
+++ Slider.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rmill_000
+ * Date: 04/11/2017
+ * Time: 15:41
+ */
+
+namespace yii\robertmill\wowslider;
+
+
+class Slider
+{
+    public function __construct()
+    {
+        return 'HI';
+    }
+}
